7d7d51166d3cdce4b49ce8ca1906b200d2391292
Ahmed Sherif <ahmedsherif4175@gmail.com>
2024-10-19 18:37:30 +0300

    init

--- lib/features/shared_features/subjects_shared_features/domain/base_repository/subjects_shared_features_base_repository.dart
+++
@@ -1,7 +0,0 @@
-part of '../../subjects_shared_features.dart';
-
-
-abstract class SubjectsSharedFeaturesBaseRepository {
-  Future<Either<Failure, List<SubjectsEntity>>> getSharedSubjects({
-    required ParameterGoToQuestions subjectsParameters,});
-}
--- lib/features/shared_features/subscriptions_and_packages/free_subjects_subscriptions/domain/free_subjects_subscriptions_base_repository/free_subjects_subscriptions_base_repository.dart
+++
@@ -1,6 +0,0 @@
-part of '../../free_subjects_subscriptions.dart';
-
-abstract class FreeSubjectsSubscriptionsBaseRepository {
-  Future<Either<Failure, List<SubjectsEntity>>> getSubjects(
-      {required FreeSubjectsSubscriptionsParameter subjectsParameters});
-}
